e2fc589c17c1c113ae0a93d75b09b636c4e385d3
elibrody <85048617+elibrody@users.noreply.github.com>
2023-11-28 11:22:05 -0500

    Create AppRegistrationHWID.ps1

---
+++ EndpointManager/Enrollment/AppRegistrationHWID.ps1
@@ -0,0 +1,25 @@
+#Required intune appregistration permissions are
+#Name = IntuneHWIDScript
+#supported account types = Single Tenant
+#DeviceManagementConfiguration.ReadWrite.All
+#DeviceManagementServiceConfig.ReadWrite.All
+#Redirect URI is web -  "https://localhost/"
+
+#Define Variables
+$TenantID = "YOUR_TENANT_ID"
+$ApplicationID = "YOUR_APP_ID"
+$ApplicationSecret = "YOUR_APP_SECRET"
+
+#Install Nuget
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+# Install Get-WindowsAutoPilotInfo module
+Install-Script -Name Get-WindowsAutopilotInfo -Force
+
+# Set execution policy to unrestricted
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
+
+#Wait 20 Seconds 
+Start-Sleep -Seconds 20
+
+# Execute Get-WindowsAutoPilotInfo command
+Get-WindowsAutoPilotInfo.ps1 -Online -TenantId $TenantID -AppId $ApplicationID -AppSecret $ApplicationSecret -GroupTag "HWIDScript"
