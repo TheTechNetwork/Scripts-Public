1adca4fbd287a39678a5e0b7ff0ae0af6a860c4d
elibrody <85048617+elibrody@users.noreply.github.com>
2024-06-04 09:40:37 -0500

    Update Run-RevoUninstallerPortable.ps1

--- Windows/Run-RevoUninstallerPortable.ps1
+++ Windows/Run-RevoUninstallerPortable.ps1
@@ -5,3 +5,4 @@ cd "C:\Temp"
 wget -o "C:\temp\revoportable.zip" -Uri "https://download.revouninstaller.com/download/RevoUninstaller_Portable.zip"
 Expand-Archive -Path "C:\temp\revoportable.zip" -DestinationPath "C:\temp\"
 cd C:\Temp\RevoUninstaller_Portable\
+.\RevoUPort.exe
