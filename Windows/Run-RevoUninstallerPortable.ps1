8ed266d5b5751b337749cc2f5295fe10cf8af132
elibrody <85048617+elibrody@users.noreply.github.com>
2024-06-04 09:39:41 -0500

    Update Run-RevoUninstallerPortable.ps1

--- Windows/Run-RevoUninstallerPortable.ps1
+++ Windows/Run-RevoUninstallerPortable.ps1
@@ -1,4 +1,4 @@
-# Run iex (New-Object Net.WebClient).DownloadString('https://github.com/TheTechNetwork/Scripts-Public/raw/752854633e6a2590b3fd11a140650d1bee8fbd29/Windows/Run-RevoUninstallerPortable.ps1')
+# Run iex (New-Object Net.WebClient).DownloadString('https://github.com/TheTechNetwork/Scripts-Public/blob/main/Windows/Run-RevoUninstallerPortable.ps1')
 
 
 cd "C:\Temp"
