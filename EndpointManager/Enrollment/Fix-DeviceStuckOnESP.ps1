80a01aada242f3fd8df58e6a2382fef16e9b1012
elibrody <85048617+elibrody@users.noreply.github.com>
2024-09-16 13:12:47 -0500

    Update Fix-DeviceStuckOnESP.ps1

--- EndpointManager/Enrollment/Fix-DeviceStuckOnESP.ps1
+++ EndpointManager/Enrollment/Fix-DeviceStuckOnESP.ps1
@@ -1,3 +1,5 @@
+#source https://call4cloud.nl/2021/06/autopilot-account-setup-identifying-security-policies/ Step 6
+
 $Name = get-childitem -path HKLM:\software\microsoft\enrollments\ -Recurse | where { $_.Property -match 'SkipUserStatusPage' }
 if ($Name)
 {
