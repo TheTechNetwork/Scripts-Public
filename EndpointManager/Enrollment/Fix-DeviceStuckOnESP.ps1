3e92e08dd797bf9d3de8fb5a48ecadde46420293
elibrody <85048617+elibrody@users.noreply.github.com>
2024-09-16 13:12:14 -0500

    Create Fix-DeviceStuckOnESP.ps1

---
+++ EndpointManager/Enrollment/Fix-DeviceStuckOnESP.ps1
@@ -0,0 +1,6 @@
+$Name = get-childitem -path HKLM:\software\microsoft\enrollments\ -Recurse | where { $_.Property -match 'SkipUserStatusPage' }
+if ($Name)
+{
+$Converted = Convert-Path $Name.PSPath
+New-ItemProperty -Path Registry::$Converted -Name SkipUserStatusPage  -Value 4294967295 -PropertyType DWORD -Force | Out-Null
+}
